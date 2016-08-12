2522a4ba2100d13d299da241379c213d3cd10e96
nikste <nikolaas.steenbergen@gmail.com>
2016-08-12 14:53:33 +0200

    removed model_training, added visualization function, other minor changes

--- purepython/src/main.py
+++ purepython/src/main.py
@@ -6,12 +6,13 @@ from data_generator import generate_noisy_polinomial_data
 from data_generator import get_mnist
 from data_generator import get_iris
 from experiments import warmstart,warmstart_parallel,train_all_parallel
-from visualizer import visualize_regression_points, visualize_warmstart_result
-
+from visualizer import visualize_regression_points, visualize_warmstart_result, visualize_training_result
 
 # train(dataset='mnist', fname='results_softmax_regression_mnist')
-train_all_parallel(dataset='mnist', fname='results_softmax_regression_mnist')
+# train_all_parallel(dataset='mnist', fname='results_softmax_regression_mnist')
 
 # warmstart(fname_in='results_softmax_regression_iris', dataset='iris', fname_out='results_softmax_regression_warmstart_iris')
 # warmstart_parallel(fname_in='results_softmax_regression_mnist', dataset='mnist', fname_out='results_softmax_regression_warmstart_mnist')
 # visualize_warmstart_result("results_softmax_regression_warmstart_mnist")
+
+visualize_training_result('/home/nikste/workspace-python/parallel-failure-recovery/experiments_results/mnist/results_softmax_regression_mnist')
--- purepython/src/visualizer.py
+++ purepython/src/visualizer.py
@@ -17,53 +17,66 @@ def visualize_training_result(filename):
     d = pickle.load(open(filename,'rb'))
 
     d_sorted = {}
+
+    # create sorted dictionary for different regularizations
     for el in d:
-        for ell in el:
-            for elll in ell:
-                r = str(elll['regularization'])
-                r_init = str(elll['initialized_with_regularization'])
-                if r not in d_sorted.keys():
-                    d_sorted[r] = {r_init: {'iterations': [elll['iterations']],
-                                            'loss_train': [elll['loss_train']],
-                                            'loss_test': [elll['loss_test']]}}
-                elif r_init not in d_sorted[r].keys():
-                    d_sorted[r][r_init] = {'iterations': [elll['iterations']],
-                                           'loss_train': [elll['loss_train']],
-                                           'loss_test': [elll['loss_test']]}  # todo: think if this is necessairy
-                else:
-                    d_sorted[r][r_init]['iterations'].append(elll['iterations'])
-                    d_sorted[r][r_init]['loss_train'].append(elll['loss_train'])
-                    d_sorted[r][r_init]['loss_test'].append(elll['loss_test'])
+        for elel in el:
+            print elel['regularization']
+            reg_k = str(elel['regularization'])
+            if reg_k not in d_sorted.keys():
+                d_sorted[reg_k] = {'iterations':[elel['iterations']],
+                                   'accuracy_train':[elel['accuracy_train']],
+                                   'accuracy_test':[elel['accuracy_test']]}
+            else:
+                d_sorted[reg_k]['iterations'].append(elel['iterations'])
+                d_sorted[reg_k]['accuracy_train'].append(elel['accuracy_train'])
+                d_sorted[reg_k]['accuracy_test'].append(elel['accuracy_test'])
 
+    print "regularization\tm_iterations\tvar_itartions\tm_accuracy_train\tvar_accuracy_train\tm_accuracy_test\tvar_accuracy_test"
+    # aggregate statistics
+    for k in list(sorted(d_sorted.keys())):
+        el = d_sorted[k]
+        m_iterations = np.mean(el['iterations'])
+        var_iterations = np.std(el['iterations'])
+
+        m_accuracy_test = np.mean(el['accuracy_test'])
+        var_accuracy_test = np.std(el['accuracy_test'])
+
+        m_accuracy_train = np.mean(el['accuracy_train'])
+        var_accuracy_train = np.std(el['accuracy_train'])
+
+        print k, "\t", m_iterations, "\t", var_iterations, "\t", \
+            m_accuracy_train, "\t", var_accuracy_train, "\t", \
+            m_accuracy_test, "\t", var_accuracy_test
     # compute mean and variance for each list ['regularization']['initialized_with_regularization']
-    k_reg = d_sorted.keys()
-    k_reg_init = d_sorted[k_reg[0]].keys()
-
-    print "reg\t", "reg_init\t", "mean\t", "variance\t", "mean_acc_train\t", "var_acc_train\t", "acc_test\t", "var_acc_test"
-
-    for k_reg in sorted(d_sorted.keys()):
-        for k_reg_init in sorted(d_sorted[k_reg].keys()):
-            # print d_sorted[k_reg][k_reg_init]['iterations']
-            m_iterations = np.mean(d_sorted[k_reg][k_reg_init]['iterations'])
-            var_iterations = np.std(d_sorted[k_reg][k_reg_init]['iterations'])
-            d_sorted[k_reg][k_reg_init]['mean_iterations'] = m_iterations
-            d_sorted[k_reg][k_reg_init]['variance_iterations'] = var_iterations
-
-            m_accuracy_test = np.mean(d_sorted[k_reg][k_reg_init]['accuracy_test'])
-            var_accuracy_test = np.std(d_sorted[k_reg][k_reg_init]['accuracy_test'])
-            d_sorted[k_reg][k_reg_init]['mean_accuracy_test'] = m_accuracy_test
-            d_sorted[k_reg][k_reg_init]['variance_accuracy_test'] = var_accuracy_test
-
-            m_accuracy_train = np.mean(d_sorted[k_reg][k_reg_init]['accuracy_train'])
-            var_accuracy_train = np.std(d_sorted[k_reg][k_reg_init]['accuracy_train'])
-            d_sorted[k_reg][k_reg_init]['mean_accuracy_train'] = m_accuracy_train
-            d_sorted[k_reg][k_reg_init]['variance_accuracy_test'] = var_accuracy_train
-
-            print k_reg, "\t", k_reg_init, "\t", m_iterations, "\t", var_iterations, "\t", \
-                m_accuracy_train, "\t", var_accuracy_train, "\t", \
-                m_accuracy_test, "\t", var_accuracy_test
-
-        print " "
+    # k_reg = d_sorted.keys()
+    # k_reg_init = d_sorted[k_reg[0]].keys()
+    #
+    # print "reg\t", "reg_init\t", "mean\t", "variance\t", "mean_acc_train\t", "var_acc_train\t", "acc_test\t", "var_acc_test"
+    #
+    # for k_reg in sorted(d_sorted.keys()):
+    #     for k_reg_init in sorted(d_sorted[k_reg].keys()):
+    #         # print d_sorted[k_reg][k_reg_init]['iterations']
+    #         m_iterations = np.mean(d_sorted[k_reg][k_reg_init]['iterations'])
+    #         var_iterations = np.std(d_sorted[k_reg][k_reg_init]['iterations'])
+    #         d_sorted[k_reg][k_reg_init]['mean_iterations'] = m_iterations
+    #         d_sorted[k_reg][k_reg_init]['variance_iterations'] = var_iterations
+    #
+    #         m_accuracy_test = np.mean(d_sorted[k_reg][k_reg_init]['accuracy_test'])
+    #         var_accuracy_test = np.std(d_sorted[k_reg][k_reg_init]['accuracy_test'])
+    #         d_sorted[k_reg][k_reg_init]['mean_accuracy_test'] = m_accuracy_test
+    #         d_sorted[k_reg][k_reg_init]['variance_accuracy_test'] = var_accuracy_test
+    #
+    #         m_accuracy_train = np.mean(d_sorted[k_reg][k_reg_init]['accuracy_train'])
+    #         var_accuracy_train = np.std(d_sorted[k_reg][k_reg_init]['accuracy_train'])
+    #         d_sorted[k_reg][k_reg_init]['mean_accuracy_train'] = m_accuracy_train
+    #         d_sorted[k_reg][k_reg_init]['variance_accuracy_test'] = var_accuracy_train
+    #
+    #         print k_reg, "\t", k_reg_init, "\t", m_iterations, "\t", var_iterations, "\t", \
+    #             m_accuracy_train, "\t", var_accuracy_train, "\t", \
+    #             m_accuracy_test, "\t", var_accuracy_test
+    #
+    #     print " "
 
 
 def visualize_warmstart_result(filename):
