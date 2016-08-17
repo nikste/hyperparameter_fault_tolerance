697633a3438ec550059df1d1e73be485c94c677a
nikste <nikolaas.steenbergen@gmail.com>
2016-08-17 18:00:03 +0200

    covertype throws logger error, removed, added partial failure experiments

--- purepython/src/main.py
+++ purepython/src/main.py
@@ -1,3 +1,7 @@
+import sys
+
+import errno
+
 from models import tf_linear_regression
 from models import tf_logistic_regression
 from experiments import train, warmstart_all_parallel, warmstart_partial_all_parallel
@@ -33,6 +37,11 @@ def full_failure_recovery(dataset, model_type):
         x, y, x_test, y_test = get_boston(train_test_ratio)
     else:
         print "warning dataset:", dataset, "not found\nexiting!"
+        try:
+            sys.stdout.flush()
+        except IOError as e:
+            if e.errno == errno.EPIPE:
+                print "broken pipe?"
         return
     f_name_train = 'results_' + model_type + '_' + dataset
     f_name_warmstart = 'results_warmstart_' + model_type + '_' + dataset
@@ -40,9 +49,19 @@ def full_failure_recovery(dataset, model_type):
     warmstart_all_parallel(x, y, x_test, y_test, fname_in=f_name_train, fname_out=f_name_warmstart, model_type=model_type, w_diff_term_crit=w_diff_term_crit, learning_rate=learning_rate, regularizations=regularizations)
     # # warmstart_all_parallel(fname_in='/home/nikste/workspace-python/parallel-failure-recovery/experiments_results/mnist/results_softmax_regression_mnist', dataset='mnist', fname_out='results_softmax_regression_warmstart_mnist')
     print "training", dataset, model_type, "full failure"
+    try:
+        sys.stdout.flush()
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            print "broken pipe?"
     visualize_training_result_from_parallel(f_name_train)
     visualize_warmstart_result_from_parallel(f_name_warmstart, f_name_train)
     print "great success!"
+    try:
+        sys.stdout.flush()
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            print "broken pipe?"
 
 
 def partial_failure_recovery(dataset, model_type):
@@ -59,6 +78,11 @@ def partial_failure_recovery(dataset, model_type):
         x, y, x_test, y_test = get_boston(train_test_ratio)
     else:
         print "warning dataset:", dataset, "not found\nexiting!"
+        try:
+            sys.stdout.flush()
+        except IOError as e:
+            if e.errno == errno.EPIPE:
+                print "broken pipe?"
         return
 
     f_name_train = 'results_' + model_type + '_' + dataset
@@ -67,9 +91,19 @@ def partial_failure_recovery(dataset, model_type):
     warmstart_partial_all_parallel(x, y, x_test, y_test, fname_in=f_name_train, fname_out=f_name_warmstart, model_type=model_type, w_diff_term_crit=w_diff_term_crit, learning_rate=learning_rate, regularizations=regularizations, corruption_ratio=0.5)
     # # warmstart_all_parallel(fname_in='/home/nikste/workspace-python/parallel-failure-recovery/experiments_results/mnist/results_softmax_regression_mnist', dataset='mnist', fname_out='results_softmax_regression_warmstart_mnist')
     print "training", dataset, model_type, "full failure"
+    try:
+        sys.stdout.flush()
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            print "broken pipe?"
     visualize_training_result_from_parallel(f_name_train)
     visualize_warmstart_result_from_parallel(f_name_warmstart, f_name_train)
     print "great success!"
+    try:
+        sys.stdout.flush()
+    except IOError as e:
+        if e.errno == errno.EPIPE:
+            print "broken pipe?"
 # train_all_parallel(dataset='diabetes', fname='results_linear_regression_diabetes', model_type='linear_regression')
 # warmstart_all_parallel(fname_in='results_linear_regression_diabetes', model_type='linear_regression', dataset='diabetes', fname_out='results_linear_regression_warmstart_diabetes')
 # # warmstart_all_parallel(fname_in='/home/nikste/workspace-python/parallel-failure-recovery/experiments_results/mnist/results_softmax_regression_mnist', dataset='mnist', fname_out='results_softmax_regression_warmstart_mnist')
@@ -79,8 +113,12 @@ def partial_failure_recovery(dataset, model_type):
 w_diff_term_crit = 0.01
 # learning_rate = 0.0001
 learning_rate = 0.000001
-# regularizations = list(reversed([100., 10., 1., 0.1, 0.01, 0.001, 0.]))
-regularizations = [0.]
+regularizations = list(reversed([100., 10., 1., 0.1, 0.01, 0.001, 0.]))
+# regularizations = [0.]
+
+# dataset = 'covertype'
+# model_type = 'softmax_regression'
+# full_failure_recovery(dataset, model_type)
 
 dataset = 'mnist'
 model_type = 'softmax_regression'
@@ -90,18 +128,35 @@ dataset = 'iris'
 model_type = 'softmax_regression'
 full_failure_recovery(dataset, model_type)
 
-dataset = 'covertype'
-model_type = 'softmax_regression'
-full_failure_recovery(dataset, model_type)
-
 dataset = 'diabetes'
 model_type = 'linear_regression'
 full_failure_recovery(dataset, model_type)
-print 'fully done!'
 
 dataset = 'boston'
 model_type = 'linear_regression'
 full_failure_recovery(dataset, model_type)
 
 print 'fully done!'
+try:
+    sys.stdout.flush()
+except IOError as e:
+    if e.errno == errno.EPIPE:
+        print "broken pipe?"
+
+dataset = 'mnist'
+model_type = 'softmax_regression'
+partial_failure_recovery(dataset, model_type)
+#
+dataset = 'iris'
+model_type = 'softmax_regression'
+partial_failure_recovery(dataset, model_type)
+
+dataset = 'diabetes'
+model_type = 'linear_regression'
+partial_failure_recovery(dataset, model_type)
+
+dataset = 'boston'
+model_type = 'linear_regression'
+partial_failure_recovery(dataset, model_type)
+
 
